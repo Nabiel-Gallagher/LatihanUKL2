08e1d844e16e30ee10502735d469a0a5660858ec
Nabiel Gallagher <deniza_fatih_34rpl@student.smktelkom-mlg.sch.id>
2025-11-03 13:49:41 +0700

    Add files via upload

--- MudahSoal2.java
+++ MudahSoal2.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class MudahSoal2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("====================================");
+        System.out.println("PROGRAM CEK BILANGAN GANJIL / GENAP");
+        System.out.println("====================================");
         System.out.println("Masukkan Angka: ");
         int angka = input.nextInt();
+        System.out.println("-----------------------------------");
 
         if(angka % 2 == 0){
             System.out.println(angka + " adalah bilangan genap");
         } else {
             System.out.println(angka + " adalah bilangan ganjil");
         }
+
+        System.out.println("===================================");
     }
 }
